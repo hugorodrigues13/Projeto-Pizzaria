4ddd40f30e161f28ba81ccdb1b5d037af0191df8
hugorodrigues13 <franciscohugo0@gmail.com>
2023-05-29 00:55:13 -0300

    tela de login-components-BUTTON

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,9 +1,41 @@
-import styles from '../styles/Home.module.scss'
+import styles from '../../styles/home.module.scss'
+import Image from 'next/image'
+import logoImg from '../../public/logo.svg'
+import Head from 'next/head'
+
+import {Input} from '../components/ui/Input'
+import {Button} from '../components/ui/Button'
 
 export default function Home(){
     return(
+        <>
+        <Head>
+            <title>Sujeito - Faça seu login</title>
+        </Head>
         <div>
-            <h1>Aprendendo NextJs </h1>
+            <Image src={logoImg} alt="Logo Sujeito Pizzaria"/>
+
+            <div className={styles.login}>
+                <form>
+                    <Input 
+                        placeholder='Digite seu email'
+                        type='text'
+                        />
+
+                    <Input 
+                        placeholder='Digite sua senha'
+                        type='password'
+                        />
+
+                    <Button
+                        type="submit"
+                        loading={false}
+                    >
+                        Cadastrar
+                    </Button>
+                </form>
+            </div>
         </div>
+        </>
     )
 }
